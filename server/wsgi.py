40403fdccbc7e3dfabe6792df0a18a42b3af21dd
cheeren <cheeren@cs.ubc.ca>
2026-03-13 10:50:59 -0700

    Revert "Add spirograph art files on top of original scaffold"
    This reverts commit bcc43f6ec4cdab91c28cb39dd7633c300b0ef454.

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,19 +1,5 @@
 import os
-from flask import Flask, send_from_directory
 
-app = Flask(__name__)
-root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from .gizmoapp_server import create_app
 
-@app.route('/')
-@app.route('/index.html')
-def index():
-    return send_from_directory(root, 'index.html')
-
-@app.route('/reveal')
-@app.route('/reveal.html')
-def reveal():
-    return send_from_directory(root, 'reveal.html')
-
-@app.route('/<path:filename>')
-def static_file(filename):
-    return send_from_directory(root, filename)
+app = create_app(shell_variant=os.getenv("GIZMOAPP_SHELL"))
